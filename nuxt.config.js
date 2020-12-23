5d45a606299eacbefa03569aec167250ce101fbe
Gabriela Brown <gabcbrown@gmail.com>
2020-12-22 17:47:42 -0800

    update configs for static deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,4 @@
 export default {
-  server: {
-    host: '0.0.0.0' // default: localhost
-  }
-  // Uncomment the following to deploy as a static site on Render
-  // target: 'static'
+  target: 'static'
 }
 
